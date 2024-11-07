b84b5546ca47a2021f98438d502217b8e32e21d8
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-11-07 19:54:36 +0600

    till m77-4 done

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,19 +1,27 @@
+import { getPosts } from '@/services/postApi';
+import Link from 'next/link';
 import React from 'react';
 
 //create data fetching function
-const getPosts = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const data = await res.json();
 
-    return data;
-}
 
 const PostPage = async () => {
     const postsData = await getPosts();
-    console.log(postsData);
+    // console.log(postsData);
     return (
         <div>
-            Post page
+            <h6>All Posts</h6>
+            <div className='grid grid-cols-4 gap-6'>
+                {
+                    postsData?.slice(0, 20)?.map(({ title, body, id }) => (
+                        <div key={id} className='border-2 p-6'>
+                            <h6>Title: {title}</h6>
+                            <h6>Description: {body}</h6>
+                            <button className='bg-red-400 px-4 py-2'><Link href={`/posts/${id}`}>See details</Link></button>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
 };
