1322e2c7f33c99c701e61d8a481804570d6e029c
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-11-11 07:56:46 +0600

    m77-v5 all meals api fetching in progress

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,29 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Meals = () => {
     const [search, setSearch] = useState('');
+    const [meals, setMeals] = useState('');
+
+    const loadData = async () => {
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`)
+        const data = await res.json();
+
+        setMeals(data);
+    }
+
+    const handler = (e) => {
+        console.log(e.target.value);
+        setSearch(e.target.value);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [search]);
+
     return (
         <div className='mt-12'>
-            <input className='p-4 outline-none border-transparent text-slate-900 bg-lime-100' type="text" placeholder='search meals...' />
+            <input onChange={handler} className='p-4 outline-none border-transparent text-slate-900 bg-lime-100' type="text" placeholder='search meals...' />
             <button className='bg-red-400 p-4'>Search</button>
         </div>
     );
