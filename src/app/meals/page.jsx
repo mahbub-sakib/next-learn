13d9ec61d53fcfc4539729e623a00acc64afa251
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-11-13 19:44:43 +0600

    module 77 complete

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,12 +1,13 @@
 import Meals from '@/components/Meals';
 import React from 'react';
+import styles from './styles.module.css'
 
 const MealsPage = () => {
 
     return (
         <div className='p-12'>
             <h1 className='text-3xl font-semibold text-red-400'>Choose Your Meals</h1>
-            <p>Choose meals by searching...</p>
+            <p className={`${styles.font_tomato} ${styles.text_large}`}>Choose meals by searching...</p>
             <Meals></Meals>
         </div>
     );
