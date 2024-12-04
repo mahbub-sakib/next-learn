2cf547e9dc5529a7e9bebfa7e47389dac876d102
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-12-04 16:47:12 +0600

    m78 v1 done

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -6,21 +6,27 @@ const getDetailsPost = async (id) => {
     return data;
 }
 
-// export const generateMetadata = async ({ params }) => {
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
-//     const postData = res.json()
+export const generateMetadata = async ({ params }) => {
+    const thisId = (await params).id;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${thisId}`)
+    const postData = await res.json()
 
-//     return {
-//         title: `Post details ${params.id}`,
-//         description: postData.body
-//     }
-// }
+    return {
+        // title: `${postData.title}`,
+        title: {
+            absolute: `${postData.title}`
+        },
+        description: postData.body,
+        keywords: postData.body.split(' ')
+    }
+}
 
 const PostDetailsPage = async ({ params }) => {
-    if (!params || !params.id) {
+    const thisId = (await params).id;
+    if (!params || !thisId) {
         throw new Error('Missing or invalid params');
     }
-    const { title, body } = await getDetailsPost(params.id)
+    const { title, body } = await getDetailsPost(thisId)
     return (
         <div className='px-12 py-24 text-3xl'>
             <h6>Title: {title}</h6>
