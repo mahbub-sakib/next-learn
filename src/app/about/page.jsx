6857d295ffc776fbc818adc1e208b0d5730b0eb5
mahbub-sakib <mahbub.sakib418@gmail.com>
2024-12-10 03:57:49 +0600

    till m78 v3 done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,10 +9,21 @@ export const metadata = {
     keywords: ["about", "about page"]
 };
 
-const page = () => {
+const getTime = async () => {
+    const res = await fetch("http://localhost:3000/time", { next: { revalidate: 5 } });
+    const data = await res.json();
+    console.log(data);
+    return data.currentTime;
+}
+
+const page = async () => {
+
+    const currentTime = await getTime();
+
     return (
         <div className={`${headland.className} min-h-screen px-12 py-24`}>
             <h6 className='text-3xl'>About Page</h6>
+            <h3 className='text-3xl text-red-400 mt-12'>Time: {currentTime}</h3>
         </div>
     );
 };
